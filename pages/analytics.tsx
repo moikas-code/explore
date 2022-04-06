afe2b56074b35f9d8b02c04b8164407d00e2c693
wcgate01 <wcgate01@louisville.edu>
2022-04-06 14:44:20 -0400

    rm unused code triggering build error

--- pages/analytics.tsx
+++ pages/analytics.tsx
@@ -6,20 +6,8 @@ export default function Analytics() {
   const connection = React.useContext(ConnectorContext);
   const blockchain = connection.sdk?.wallet?.blockchain;
   const router = useRouter();
-  useEffect(() => {
-    connection.state.status === 'connected' && router.push('/');
-  }, [connection]);
-  if (connection.state.status == 'initializing') {
-    return <div>Loading...</div>;
-  }
-  if (connection.state.status == 'disconnected') {
-    return (
-      <div className='h-100 d-flex flex-column justify-content-center align-items-center'>
-       
-         }
-        
-      </div>
-    );
-  }
-  return null;
+
+  return (
+    <div className='h-100 d-flex flex-column justify-content-center align-items-center'></div>
+  );
 }
